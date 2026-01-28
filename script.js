5ad66f59cdcbf2418df83488a52905601f9a0d59
shanesmith1136-cpu <shanesmith1136@gmail.com>
2026-01-28 13:25:40 +0000

    Add theme toggle

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const form = document.getElementById("contact-form");
 const message = document.getElementById("message");
+const themeBtn = document.getElementById("theme-btn");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   const name = document.getElementById("name").value;
-
   message.textContent = `Thanks, ${name}! Your message was sent 🚀`;
+});
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
 });
